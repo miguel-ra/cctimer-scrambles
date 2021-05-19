1b20cdbd1a49b1cb9d88bdb69875daad5b28af6d
Miguel Rordríguez <mrodriguez@fintonic.com>
2021-05-19 23:32:45 +0200

    Fix scramble redraw

--- components/CanvasFrame.js
+++ components/CanvasFrame.js
@@ -5,16 +5,19 @@ function CanvasFrame({ scramble, randomScramble, width, height, ...props }) {
   const [element, setSelement] = (0, useState)(null);
   (0, useEffect)(() => {
     if (element) {
+      element.innerHTML = "";
       scramble.drawScramble(element, randomScramble.state);
     }
-  }, [element]);
+  }, [element, randomScramble]);
   return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", { ...props,
     children: /*#__PURE__*/(0, _jsxRuntime.jsx)("canvas", {
       width: width || "100%",
       height: height,
-      ref: element => {
-        setSelement(element);
-      }
+      ref: (elementRef) => {
+        if (!element && elementRef) {
+          setSelement(elementRef);
+        }
+      },
     })
   });
 }
--- components/RaphaelFrame.js
+++ components/RaphaelFrame.js
@@ -5,12 +5,15 @@ function RaphaelFrame({ scramble, randomScramble, width, height, ...props }) {
   const [element, setSelement] = (0, useState)(null);
   (0, useEffect)(() => {
     if (element) {
+      element.innerHTML = "";
       scramble.drawScramble(element, randomScramble.state, width, height);
     }
-  }, [element]);
+  }, [element, randomScramble, width, height]);
   return /*#__PURE__*/ (0, _jsxRuntime.jsx)("div", {
-    ref: (element) => {
-      setSelement(element);
+    ref: (elementRef) => {
+      if (!element && elementRef) {
+        setSelement(elementRef);
+      }
     },
     ...props,
   });
